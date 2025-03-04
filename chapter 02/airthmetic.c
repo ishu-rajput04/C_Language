85cfd15649bd7f27a23bcdc21f4b4e614ec09fe8
ishu-rajput04 <official.ishurajput04@gmail.com>
2025-03-04 12:27:07 +0530

    Add files via upload

---
+++ chapter 02/airthmetic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    printf("the sum of a and b is :%d\n",a+b);
+    printf("the sub of a and b is :%d\n",a-b);
+    printf("the multiple of a and b is :%d\n",a*b);
+    printf("the division of a and b is :%d\n",a/b);
+    printf("the modulus of a and b is :%d\n",a%b);
+    
+    return 0;
+}
